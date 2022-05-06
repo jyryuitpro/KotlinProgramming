7848a79b2140dfe7a9d88c98457096ffaf84db6b
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-05-06 14:54:26 +0900

    50. 11-2 코루틴 개념과 사용 (1)

---
+++ src/chap11/section02/BasicCoroutine.kt
@@ -0,0 +1,14 @@
+package chap11.section02
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main() {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World")
+    }
+    println("Hello")
+    Thread.sleep(2000L)
+}
